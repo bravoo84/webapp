8bb1a647cd8fc180b677e0e5f543d2a2a5f8cb07
bhavik <bhavikmehta98@gmail.com>
2019-10-11 19:18:47 +0530

    trial rounds

---
+++ src/main/java/com/bhavik/trial/test.java
@@ -0,0 +1,29 @@
+package com.bhavik.trial;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		String url = "jdbc:mysql://localhost:3306/netbanking?useSSl=false&serverTimezone=UTC";
+		String user="netbanking";
+		String pass="netbanking";
+		/*
+		 * */
+		try {
+			System.out.println("Connecting...!");
+			
+			Connection myConn = DriverManager.getConnection(url,user,pass);
+			
+			System.out.println("Connection successful!!");
+			
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
